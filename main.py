5e3235cda9e0e3e87f2851f4a7985e9b3488c31c
Jeffery Erhunse <>
2021-06-02 16:02:56 +0000

    Added comments to code

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ print(f"FYI the number is {guess}")
 # Allow the player to submit a guess for a number between 1 and 100.
 difficulty = input("Choose a difficulty 'easy' or 'hard': ")
 
+# If they got the answer correct, show the actual answer to the player.
 # Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
 if difficulty == "easy":
   guess_left = 10
@@ -31,7 +32,11 @@ if difficulty == "easy":
       print("you ran out of attempts, you lose")
       print(f"The number was {guess}")
 
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player. 
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
+#Hard difficulty settings 
 if difficulty == "hard":
   guess_left = 5
   print(f"You have {guess_left} attempts left to guess the number")
@@ -49,8 +54,6 @@ if difficulty == "hard":
     if guess_left == 0:
       print("you ran out of attempts, you lose")
 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+
+
 
